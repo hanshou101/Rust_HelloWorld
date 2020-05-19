794338a6e0fcff16bf74eb8f199f271d80ab4aea
yanchongwen <yanchongwen101@163.com>
2020-05-19 14:06:35 +0800

    ① 为【Rust】打包成【WebAssembly】，添加了部分代码支持。（尚未完全完成）

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,93 @@
-use std::num::ParseIntError;
-use std::ops::Mul;
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+// use std::num::ParseIntError;
+// use std::ops::Mul;
 use std::cmp;
 
-fn main() {
-    test____float2Fixed();
-    test____digitLength();
-    test____times();
-    test____divide();
-    test____plus();
-    test____minus();
+/*
+
+#[wasm_bindgen]
+pub fn main() {
+test____float2Fixed();
+test____digitLength();
+test____times();
+test____divide();
+test____plus();
+test____minus();
 }
 
 fn test____float2Fixed() {
-    println!("test____float2Fixed");
-    let str = "0.12345789";
-    let int_a = match str.parse::<f64>() {
-        Ok(n) => { float2Fixed(n) }
-        Err(_) => 0,
-    };
-    println!("{}", int_a);
+println!("test____float2Fixed");
+let str = "0.12345789";
+let int_a = match str.parse::<f64>() {
+    Ok(n) => { float2Fixed(n) }
+    Err(_) => 0,
+};
+println!("{}", int_a);
 }
 
 fn test____digitLength() {
-    println!("test____digitLength");
-    let str = "0.12345789";
-    let a = digitLength(str);
-    println!("{}", a);
+println!("test____digitLength");
+let str = "0.12345789";
+let a = digitLength(str);
+println!("{}", a);
 }
 
 fn test____times() {
-    println!("test____times");
-    println!("{}", times(21_0000.0, 1_0000.0, vec![&1000.0, &8.2]));        // 常见出错值1
-    println!("{}", times(0.012345, 0.000001, vec![]));                      // 常见出错值2
-    println!("{}", times(512.06, 100.0, vec![]));                           // 常见出错值3
-    println!("{}", times(0.11, 1.1, vec![]));                           // 常见出错值3
-    println!("{}", times(1.11, 10.0, vec![]));                           // 常见出错值3
-    println!("{}", times(512.06, 100.0, vec![]));                           // 常见出错值3
-    println!("{}", times(1.02, 0.05, vec![]));                           // 常见出错值3
-    println!("{}", times(575697.82, 100.0, vec![]));                           // 常见出错值3
-    println!("{}", times(0.1, 0.2, vec![]));                           // 常见出错值3
-    println!("{}", times(2090.5, 8.61, vec![]));                           // 常见出错值3
-    println!("{}", times(0.2, 0.4, vec![]));                           // 常见出错值3
-    println!("{}", times(6.0, 0.2, vec![]));                           // 常见出错值3
-    println!("{}", times(200.3, 3.0, vec![]));                           // 常见出错值3
-    println!("{}", times(600.9, 3.0, vec![]));                           // 常见出错值3
-    //
-    println!("{}", times(0.0000_0000_1, 0.0000_0000_2, vec![]));            // 超小值 * 超小值
-    println!("{}", times(0.0000_0000_1, 1_0000_0000_2.0, vec![]));          // 超小值 * 超大值
-    println!("{}", times(123456789.123456789, 123456789.123456789, vec![]));// 超大值 * 超大值
+println!("test____times");
+println!("{}", times(21_0000.0, 1_0000.0, vec![&1000.0, &8.2]));        // 常见出错值1
+println!("{}", times(0.012345, 0.000001, vec![]));                      // 常见出错值2
+println!("{}", times(512.06, 100.0, vec![]));                           // 常见出错值3
+println!("{}", times(0.11, 1.1, vec![]));                           // 常见出错值3
+println!("{}", times(1.11, 10.0, vec![]));                           // 常见出错值3
+println!("{}", times(512.06, 100.0, vec![]));                           // 常见出错值3
+println!("{}", times(1.02, 0.05, vec![]));                           // 常见出错值3
+println!("{}", times(575697.82, 100.0, vec![]));                           // 常见出错值3
+println!("{}", times(0.1, 0.2, vec![]));                           // 常见出错值3
+println!("{}", times(2090.5, 8.61, vec![]));                           // 常见出错值3
+println!("{}", times(0.2, 0.4, vec![]));                           // 常见出错值3
+println!("{}", times(6.0, 0.2, vec![]));                           // 常见出错值3
+println!("{}", times(200.3, 3.0, vec![]));                           // 常见出错值3
+println!("{}", times(600.9, 3.0, vec![]));                           // 常见出错值3
+//
+println!("{}", times(0.0000_0000_1, 0.0000_0000_2, vec![]));            // 超小值 * 超小值
+println!("{}", times(0.0000_0000_1, 1_0000_0000_2.0, vec![]));          // 超小值 * 超大值
+println!("{}", times(123456789.123456789, 123456789.123456789, vec![]));// 超大值 * 超大值
 }
 
 fn test____minus() {
-    println!("test____minus");
-    println!("{}", minus(1.1, 1.0, vec![]));
-    println!("{}", minus(57168.619999999995, 11087.28, vec![]));
-    println!("{}", minus(-0.09, 0.01, vec![]));
-    println!("{}", minus(2.0, 1.10, vec![]));
-    println!("{}", minus(0.3, 0.2, vec![]));
+println!("test____minus");
+println!("{}", minus(1.1, 1.0, vec![]));
+println!("{}", minus(57168.619999999995, 11087.28, vec![]));
+println!("{}", minus(-0.09, 0.01, vec![]));
+println!("{}", minus(2.0, 1.10, vec![]));
+println!("{}", minus(0.3, 0.2, vec![]));
 }
 
 fn test____plus() {
-    println!("test____plus");
-    println!("{}", plus(0.1, 0.2, vec![]));
-    println!("{}", plus(0.28, 0.34, vec![]));
-    println!("{}", plus(2412.02, 11087.64, vec![&8338.28, &5580.0]));
-    println!("{}", plus(2.28, 2.34, vec![]));
-    println!("{}", plus(33.28, 3.34, vec![]));
-    println!("{}", plus(3.28, 3.34, vec![]));
-    println!("{}", plus(4.28, 4.34, vec![]));
-    println!("{}", plus(5.28, 5.34, vec![]));
-    println!("{}", plus(8.28, 8.34, vec![]));
-    println!("{}", plus(33.28, 9.34, vec![]));
+println!("test____plus");
+println!("{}", plus(0.1, 0.2, vec![]));
+println!("{}", plus(0.28, 0.34, vec![]));
+println!("{}", plus(2412.02, 11087.64, vec![&8338.28, &5580.0]));
+println!("{}", plus(2.28, 2.34, vec![]));
+println!("{}", plus(33.28, 3.34, vec![]));
+println!("{}", plus(3.28, 3.34, vec![]));
+println!("{}", plus(4.28, 4.34, vec![]));
+println!("{}", plus(5.28, 5.34, vec![]));
+println!("{}", plus(8.28, 8.34, vec![]));
+println!("{}", plus(33.28, 9.34, vec![]));
 }
 
 fn test____divide() {
-    println!("test____divide");
-    println!("{}", divide(0.000001, 0.0001, vec![]));
-    println!("{}", divide(0.3, 0.2, vec![]));
+println!("test____divide");
+println!("{}", divide(0.000001, 0.0001, vec![]));
+println!("{}", divide(0.3, 0.2, vec![]));
 }
 
+
+*/
 
 //
 //
@@ -107,7 +116,8 @@ fn test____divide() {
 /// 精确减法
 ///     1.
 ///
-fn minus(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
+#[wasm_bindgen]
+pub fn minus(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
     if vars.len() > 0 {
         return minus(minus(a, b, vec![]), *vars[0],
                      vars.clone().splice(1.., &[]).collect(),
@@ -126,7 +136,8 @@ fn minus(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
 /// 精确加法
 ///     1.
 ///
-fn plus(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
+#[wasm_bindgen]
+pub fn plus(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
     if vars.len() > 0 {
         return plus(plus(a, b, vec![]), *vars[0],
                     vars.clone().splice(1.., &[]).collect(),
@@ -151,7 +162,8 @@ fn plus(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
 /// 精确除法
 ///     1.
 ///
-fn divide(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
+#[wasm_bindgen]
+pub fn divide(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
     if vars.len() > 0 {
         return divide(divide(a, b, vec![]), *vars[0],
                       // // 此处，简单的克隆并截取一下。然后取返回值（被替换掉的一段）
@@ -179,7 +191,8 @@ fn divide(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
 /// 精确乘法
 ///     1.对于Vec的性能，可以做进一步优化
 ///
-fn times(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
+#[wasm_bindgen]
+pub fn times(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
     if vars.len() > 0 {
         return times(times(a, b, vec![]), *vars[0],
                      // // 此处，简单的克隆并截取一下。然后取返回值（被替换掉的一段）
@@ -240,7 +253,8 @@ fn times(a: f64, b: f64, vars: Vec<&f64>) -> f64 {
 ///
 /// 转化【小数】为【整数】
 ///
-fn float2Fixed(num: f64) -> i64 {
+#[wasm_bindgen]
+pub fn float2Fixed(num: f64) -> i64 {
     let str = num.to_string();
     println!("          float2Fixed str {}", str);
     let isSci: bool = str.contains('e') || str.contains('E');
@@ -263,7 +277,8 @@ fn float2Fixed(num: f64) -> i64 {
 ///     1.此处，采用纯字符串，而不是正则分割
 ///         1.原因：更加节省空间。
 ///
-fn digitLength(str: &str) -> i8 {
+#[wasm_bindgen]
+pub fn digitLength(str: &str) -> i8 {
     let eSplit: Vec<&str> = str.split(|c| c == 'e' || c == 'E').collect();
 
     let front = match eSplit.get(0) {
